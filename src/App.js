81d86a6e83a7c0e1f964f37a71f44c241f5214a2
AbelOsaretin <contact.abel321@gmail.com>
2024-04-26 13:28:42 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,197 @@
+import "./App.css";
+import { ethers } from "ethers";
+import { abi, contractAddress } from "./constants.js";
+
+function App() {
+  async function connectButton() {
+    //let signer = null;
+    let provider;
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      console.log(provider);
+
+      //signer = await provider.getSigner();
+    }
+    console.log("Wallet Connected..........................");
+  }
+
+  // async function disconnectButton() {
+  //   console.log("Wallet Disconnected.......................");
+  // }
+
+  async function Deposit() {
+    let signer = null;
+    let provider;
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      console.log(provider);
+      signer = await provider.getSigner();
+    }
+
+    try {
+      const newDeposit = document.getElementById("depositAmount").value;
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      // const formatedDeposit = new ethers.parseUnits(`${newDeposit}`, "6");
+      const transaction = await contract.Deposit(newDeposit);
+      await transaction.wait();
+      console.log("Amount Deposited!", transaction);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  let addressesArray = [];
+  let amountArray = [];
+
+  function addAddress() {
+    try {
+      const newPayoutAdderss = document.getElementById("payoutAddress").value;
+
+      addressesArray = newPayoutAdderss.split(/\s+/);
+
+      console.log("Addresses Added :", addressesArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function addAmount() {
+    try {
+      const newPayoutAmount = document.getElementById("payoutAmount").value;
+      // amountArray.push(newPayoutAmount);
+      amountArray = newPayoutAmount.split(/\s+/);
+      console.log("Amounts Added :", amountArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  //Upda
+
+  async function Payout() {
+    addAddress();
+    addAmount();
+    let signer = null;
+    let provider;
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      console.log(provider);
+      signer = await provider.getSigner();
+    }
+
+    try {
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      // const formatedDeposit = new ethers.parseUnits(`${newDeposit}`, "6");
+      const transaction = await contract.sendUSDC(addressesArray, amountArray);
+      await transaction.wait();
+      console.log("Payout Transaction!", transaction);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function viewDetails() {
+    let provider;
+    if (window.ethereum == null) {
+      console.log("MetaMask not installed; using read-only defaults");
+      provider = ethers.getDefaultProvider();
+    } else {
+      provider = new ethers.BrowserProvider(window.ethereum);
+      console.log(provider);
+      await provider.send("eth_requestAccounts", []);
+
+      try {
+        const contract = new ethers.Contract(contractAddress, abi, provider);
+        const transaction = await contract.Administrators(0);
+        const transaction1 = await contract.Administrators(1);
+        const transaction2 = await contract.Administrators(2);
+        console.log(transaction);
+        console.log(transaction1);
+        console.log(transaction2);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  async function PayoutV2() {
+    // const jsonData = {
+    //   addresses: [
+    //     "0x2fd1AFA939eFD359a302D757740d6eC15b820bC2",
+    //     "0x725b33A7d6344744bb1bAbbF8D11F204d018a586",
+    //     "0xF7c371Ea75F648cC3070B3f538e0Bd68359FEDc2",
+    //   ],
+    //   amounts: [1000000, 4000000, 2000000],
+    // };
+
+    try {
+      // Fetch JSON data from an external source
+      const response = await fetch(
+        "https://raw.githubusercontent.com/AbelOsaretin/Payout-Frontend-V3/master/data.json"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const jsonData = await response.json();
+
+      // Extract addresses and amounts from JSON
+      const addresses = jsonData.addresses;
+      const amounts = jsonData.amounts;
+      console.log("Address and amount", addresses, amounts);
+
+      let signer = null;
+      let provider;
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults");
+        provider = ethers.getDefaultProvider();
+      } else {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        console.log(provider);
+        signer = await provider.getSigner();
+      }
+
+      try {
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        // const formatedDeposit = new ethers.parseUnits(`${newDeposit}`, "6");
+        const transaction = await contract.sendUSDC(addresses, amounts);
+        await transaction.wait();
+        console.log("Payout Transaction!", transaction);
+      } catch (error) {
+        console.log(error);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <br />
+        <div>Sample Implementation</div>
+        <button onClick={connectButton}>Connect Wallet</button>
+        <br />
+        {/* <button onClick={disconnectButton}>Disconnect Wallet</button> */}
+        <label for="depositAmount">Enter Deposit Amount</label>
+        <input id="depositAmount"></input>
+
+        <button onClick={Deposit}>Deposit</button>
+        <br />
+        <h1> Payout </h1>
+
+        <button onClick={PayoutV2}>Payout</button>
+        <br />
+        <button onClick={viewDetails}>View ADMINS</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,302 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_admins",
+        type: "address",
+      },
+    ],
+    name: "AddAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "AdminWithdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "Deposit",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_admins",
+        type: "address",
+      },
+    ],
+    name: "RemoveAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_usdcAddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_itAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "DepositSuccessful",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "Payer",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address[]",
+        name: "Payees",
+        type: "address[]",
+      },
+      {
+        indexed: true,
+        internalType: "uint256[]",
+        name: "Amount",
+        type: "uint256[]",
+      },
+    ],
+    name: "PayoutSuccessful",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "recipients",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "amounts",
+        type: "uint256[]",
+      },
+    ],
+    name: "sendUSDC",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "Withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "WithdrawalSuccessful",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "Balance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "GetAllAdminWithdrawals",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "withdrawals",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Payout.AdminWithdrawalsAndAmount[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "GetAllAmountsWithdrawnByPayee",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "payees",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Payout.PayeeAndAmountWithdrawn[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "GetAllDeposits",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "depositors",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Payout.DepositorsAndAmount[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "GetAllPayees",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "payees",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Payout.PayeesAndAmount[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "USDCAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+export const contractAddress = "0xB4f8709064751078CbaE907F57330910f570bbbe";
